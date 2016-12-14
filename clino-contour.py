9101f6f21bda1b285bcceb1add78522c8cd5b74f
Nick Farnham <brythonick@gmail.com>
2016-12-14 20:21:58 +0000

    Initial commit of script

---
+++ clino-contour.py
@@ -0,0 +1,81 @@
+"""
+================================================================================
+INCLINOMETER PLOTTING
+================================================================================
+Plot data from a borehole inclinometer as a contour graph. Data is provided as
+CSV files in the same directory as the script. Plots are also output to the same
+directory as PNG files.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from datetime import datetime
+from csv import reader
+from os import listdir, chdir
+from os.path import isfile, join, dirname, abspath
+
+
+# ============================================================================ #
+# FUNCTIONS
+# ============================================================================ #
+def is_csv(path):
+    if isfile(path) and ".csv" in path:
+        return True
+    else:
+        return False
+
+
+def get_csv_filenames(path):
+    return [file for file in listdir(path) if is_csv(join(path, file))]
+
+
+def read_csv_file(path):
+    with open(path) as csv_file:
+        return [row for row in reader(csv_file, delimiter=",")]
+
+
+def x_axis(headers):
+    str_dates = [date.split(" ")[0] for date in headers[1:]]
+    date_objs = [datetime.strptime(date, "%d/%m/%Y") for date in str_dates]
+    date_objs.sort()
+    return [(date - date_objs[0]).days for date in date_objs]
+
+
+def y_axis(data):
+    return [float(row[0]) for row in data[1:]]
+
+
+def z_data(data):
+    return np.array([row[1:] for row in data[1:]], dtype=np.float64)
+
+
+def generate_plot(x, y, z):
+    fig = plt.figure(figsize=(6, 4), dpi=300)
+    colour_plot = plt.contourf(x, y, z, 10)
+    key = plt.colorbar(colour_plot)
+    key.ax.set_ylabel("Inclination")
+    plt.title("Inclinometer Contour Plot")
+    plt.xlabel("Days")
+    plt.ylabel("Depth (m)")
+    plt.gca().invert_yaxis()
+    fig.set_tight_layout(True)
+    return plt
+
+
+def plot(file):
+    data = read_csv_file(file)
+    contour = generate_plot(
+        x_axis(data[0]),
+        y_axis(data),
+        z_data(data)
+    )
+    contour.savefig(join(".", "{0}.png".format(file.rstrip(".csv"))), dpi=300)
+
+
+# ============================================================================ #
+# MAIN BLOCK
+# ============================================================================ #
+if __name__ == "__main__":
+    script_path = dirname(abspath(__file__))
+    chdir(script_path)
+    [plot(file) for file in get_csv_filenames(".")]
